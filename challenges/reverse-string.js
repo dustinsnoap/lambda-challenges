01e7b56fbd642e7e45cdc4784d28df874d5cd277
dustinsnoap <afrobacon@gmail.com>
2019-01-22 22:01:07 -0500

    added challenge 2

---
+++ challenges/reverse-string.js
@@ -0,0 +1,17 @@
+/*
+Good morning! Write a function called reverseString that accepts a string and returns a reversed copy of the string.
+
+Input Example:
+'hello world'
+'asdf'
+'CS rocks!'
+
+Output Example:
+'dlrow olleh'
+'fdsa'
+'!skcor SC'
+*/
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverseString(`abc123`));
